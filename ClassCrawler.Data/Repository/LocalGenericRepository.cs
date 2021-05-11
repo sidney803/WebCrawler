158392264aecd8d6e236285a2d399727760eff76
星劭 劉 <sidneyliu@gmail.com>
2021-05-11 09:29:41 +0800

    修bug

---
+++ ClassCrawler.Data/Repository/LocalGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ClassCrawler.Data.Models;
+
+namespace ClassCrawler.Data.Repository
+{
+    public class LocalGenericRepository 
+    {
+        private readonly ClassDbContext _dbContext;
+
+        public LocalGenericRepository()
+        {
+            //_dbContext = new ClassDbContext();
+            _dbContext = new ClassDbContext();
+        }
+
+        public async Task CreateAsync(ClassInfo entity)
+        {
+            //await _dbContext.Set<IEnumerable<ClassInfo>>().AddAsync(entity);
+            await _dbContext.ClassInfo.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+    }
+}
